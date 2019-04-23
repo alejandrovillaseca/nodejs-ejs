0730b43f6664b2511bfd1c2c0ec718d56658772b
Rios Espinoza <s5098547@chl.bns>
2019-04-23 11:28:35 -0400

    inicializacion

---
+++ api-rest/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var server = app.listen(8082, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("App listening at http://%s:%s", host, port)
+})
+
+app.get('/listUsers', function (req, res) {
+    console.log("dirName: " + __dirname)
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
+})
+
+app.post('/addUser', function (req, res) {
+    // First read existing users. 
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data); data["user4"] = user["user4"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
+
+app.get('/:id', function (req, res) {
+    // First read existing users. 
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data); var user = users["user" + req.params.id]
+        console.log(user);
+        res.end(JSON.stringify(user));
+    });
+})
+
+app.delete('/deleteUser', function (req, res) {
+    // First read existing users. 
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data); delete data["user" + 2];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
